12df309affa400533d016ede1505330444f846e8
Tu Nombre <gabriel.dehner@rocketlab.ai>
2022-05-03 22:32:05 -0300

    Support xls is added

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import fs from 'fs'
 import qrcode from 'qrcode-terminal'
 import ClientWhatsappService from './src/services/ClientWhatsappService'
 import { getMessage } from './src/services/MessageService'
-import { proccessFile } from './src/services/ProccessFileService'
+import { proccessFile, proccessFileXls } from './src/services/ProccessFileService'
 import { NameAndPhone } from './src/models/NameAndPhoneInterface'
 
 const clientWhatsappService = new ClientWhatsappService()
@@ -29,6 +29,24 @@ router.post('/send-message', upload.single('file'), async (req: any, response) =
     flag: 'r',
   })
   const arrayToSendMessage: NameAndPhone[] = await proccessFile(req.file.path)
+  for (const element of arrayToSendMessage) {
+    await clientWhatsappService.sendMessage(`54${element.phone}`, getMessage(element.name, file))
+  }
+  response.json(true)
+})
+
+router.post('/send-message-xls', upload.single('file'), async (req: any, response) => {
+  if (!clientWhatsappService.isReady) {
+    response.json(false)
+    await fs.unlinkSync(req.file.path)
+    return
+  }
+  const pathFile = path.join(__dirname, './src/configs/MessageDefault.txt')
+  const file = await fs.readFileSync(pathFile, {
+    encoding: 'utf8',
+    flag: 'r',
+  })
+  const arrayToSendMessage: NameAndPhone[] = await proccessFileXls(req.file.path)
   for (const element of arrayToSendMessage) {
     await clientWhatsappService.sendMessage(`54${element.phone}`, getMessage(element.name, file))
   }
--- src/services/ProccessFileService.ts
+++ src/services/ProccessFileService.ts
@@ -1,6 +1,8 @@
 import { Env } from '../configs/config'
 import { NameAndPhone } from '../models/NameAndPhoneInterface'
 import fs from 'fs'
+import xlsx from 'node-xlsx'
+
 
 export const proccessFile = async (pathName: string): Promise<NameAndPhone[]> => {
   return new Promise((resolve, reject) => {
@@ -19,31 +21,9 @@ export const proccessFile = async (pathName: string): Promise<NameAndPhone[]> =>
             }
             return split
           })
-          const filterArray = filterNameAndPhone(procInColumn)
-          filterArray.forEach((element) => {
-            element.name = element.name.split('\n')[1]
-            element.phone = element.phone.split('\n')[1]
-            element.phone = element.phone.split(' ')[0].replace('-', '')
-            if (element.phone[0] === '5' && element.phone[1] === '4') {
-              element.phone = element.phone.substring(2)
-            }
-            if (
-              element.phone[0] === '1' &&
-              element.phone[1] === '5' &&
-              element.phone[2] === '4' &&
-              element.phone.length === 9
-            ) {
-              element.phone = element.phone.substring(2)
-              element.phone = `376${element.phone}`
-            }
-
-            if (Env('ENVIRONMENT') === 'TEST') {
-              element.phone = `3764639284`
-            }
-          })
-          const lastProcessArray = filterArray.filter((elem) => elem.name && elem.phone)
+          const dataProccesed = proccessFormatRow(procInColumn)
           await fs.unlinkSync(pathName)
-          resolve(lastProcessArray)
+          resolve(dataProccesed)
         } catch (err: any) {
           console.log('Error in process file... ', err.message)
           resolve([])
@@ -64,4 +44,44 @@ const filterNameAndPhone = (array: string[][]): NameAndPhone[] => {
     i++
   }
   return arrayWithProperties
+}
+
+
+export const proccessFileXls = async (pathName: string): Promise<NameAndPhone[]> => {
+  return new Promise((resolve, reject) => {
+    console.log("llego")
+    const objectXls = xlsx.parse(pathName); // parses a file
+    const data = objectXls?.[0].data as string[][]
+    const dataProcessed = proccessFormatRow(data)
+    fs.unlinkSync(pathName)
+    resolve(dataProcessed)
+
+  })
+}
+
+const proccessFormatRow = (data: string[][])=>{
+  const filterArray = filterNameAndPhone(data)
+  filterArray.forEach((element) => {
+    element.name = element.name.split('\n')[1]
+    element.phone = element.phone.split('\n')[1]
+    element.phone = element.phone.split(' ')[0].replace('-', '')
+    if (element.phone[0] === '5' && element.phone[1] === '4') {
+      element.phone = element.phone.substring(2)
+    }
+    if (
+      element.phone[0] === '1' &&
+      element.phone[1] === '5' &&
+      element.phone[2] === '4' &&
+      element.phone.length === 9
+    ) {
+      element.phone = element.phone.substring(2)
+      element.phone = `376${element.phone}`
+    }
+
+    if (Env('ENVIRONMENT') === 'TEST') {
+      element.phone = `3764639284`
+    }
+  })
+  const lastProcessArray = filterArray.filter((elem) => elem.name && elem.phone)
+  return lastProcessArray
 }
